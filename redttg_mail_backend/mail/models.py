525801d0fdb6cfc4ef7a7d7a3f1810422c967247
Red <red@redttg.com>
2023-09-28 19:00:22 +0000

    setup attachments

--- redttg_mail_backend/mail/models.py
+++ redttg_mail_backend/mail/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 
-# TODO: I need to test the api first
-# class Mail(models.Model):
-#     subject = models.CharField(max_length=255)
-#     body = models.TextField()
-#     from_email = models.EmailField()
-#     to_email = models.EmailField()
-#     created = models.DateTimeField(auto_now_add=True)
+class Mail(models.Model):
+    data = models.TextField()
+
+
+class Attachment(models.Model):
+    file = models.ForeignKey('File', on_delete=models.CASCADE)
+    filename = models.CharField(max_length=255)
+    mail = models.ForeignKey('Mail', on_delete=models.CASCADE, related_name='attachments')
+
+class File(models.Model):
+    file = models.FileField(upload_to='attachments_files/')
+    md5_hash = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.md5_hash
