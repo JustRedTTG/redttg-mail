3e116ce5eb358ba95e99a94951a29cf754af5610
Red <red@redttg.com>
2023-09-28 21:43:53 +0300

    some changes to the attachments

--- redttg_mail_backend/mail/models.py
+++ redttg_mail_backend/mail/models.py
@@ -4,3 +4,11 @@ from django.db import models
 # TODO: I need to test the api first
 class Mail(models.Model):
     data = models.TextField()
+
+
+class Attachment(models.Model):
+    file = models.FileField(upload_to='attachments/')
+    md5_hash = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.md5_hash
--- redttg_mail_backend/mail/views.py
+++ redttg_mail_backend/mail/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from .models import Mail
+
+from .file_functions import calculate_md5
+from .models import Mail, Attachment
 
 
 @csrf_exempt
@@ -9,7 +11,19 @@ from .models import Mail
 def mail(request: HttpRequest):
     d = dict(request.POST)
 
-    print(request.FILES)
+    for file in request.FILES:
+        md5_hash = calculate_md5(file)
+
+        # Check if a file with the same hash exists in the database
+        existing_attachment = Attachment.objects.filter(md5_hash=md5_hash).first()
+
+        if not existing_attachment:
+            # Create a new Attachment record
+            new_attachment = Attachment(file=file, md5_hash=md5_hash)
+            new_attachment.save()
+
+
+
 
     Mail.objects.create(data=d).save()
 
