4c0005cd185accb9f65037550df6c3b7c287fb0b
Red <red@redttg.com>
2023-09-30 12:20:58 +0000

    update urls to have variable content

--- redttg_mail_backend/mail/urls.py
+++ redttg_mail_backend/mail/urls.py
@@ -1,7 +1,8 @@
+import os
 from django.urls import path
 
 from .views import mail
 
 urlpatterns = [
-    path('', mail),
+    path(os.environ.get("EXTERNAL_API_MAIL", ''), mail),
 ]
--- redttg_mail_backend/urls.py
+++ redttg_mail_backend/urls.py
@@ -1,6 +1,7 @@
+import os
 from django.urls import path, include
 
 urlpatterns = [
-    path('auth/', include('redttg_mail_backend.auth_app.urls')),
-    path('mail/', include('redttg_mail_backend.mail.urls')),
+    path(os.environ.get("EXTERNAL_API_AUTH", ''), include('redttg_mail_backend.auth_app.urls')),
+    path('', include('redttg_mail_backend.mail.urls')),
 ]
