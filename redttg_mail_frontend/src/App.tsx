aa4a7fc66c41611f519ce1ebea55923b33965658
Red <red@redttg.com>
2023-09-30 21:27:52 +0000

    fix App.tsx

--- redttg_mail_frontend/src/App.tsx
+++ redttg_mail_frontend/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      {/* <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} /> */}
     </Routes>
   );
 }
