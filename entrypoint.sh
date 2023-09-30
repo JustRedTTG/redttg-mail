c005c81292e2231cb6b103f2b357e9eb5368110c
Red <red@redttg.com>
2023-09-30 13:13:16 +0000

    revert

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo "test"
-exec "$@"
-# echo "Waiting for postgres..."
-# /app/wait-for-it.sh $DB_HOST:$DB_PORT -- /app/run.sh "$@"
+echo "Waiting for postgres..."
+/app/wait-for-it.sh $DB_HOST:$DB_PORT -- /app/run.sh "$@"
