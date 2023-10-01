1cf6f5385177a2e342b3fc3057b073101b9d988a
Red <red@redttg.com>
2023-10-01 10:48:14 +0000

    remove slash

--- redttg_mail_backend/auth_app/urls.py
+++ redttg_mail_backend/auth_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import auth, login_api#, register
 
 urlpatterns = [
-    path('/', auth),
+    path('', auth),
     path('login/', login_api),
     # path('register/', register),
 ]
