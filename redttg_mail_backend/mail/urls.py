81d822b3e02e72f3cf018efda0071ea4c50f0e1b
Red <red@redttg.com>
2023-10-01 10:16:18 +0000

    fix url slash

--- redttg_mail_backend/mail/urls.py
+++ redttg_mail_backend/mail/urls.py
@@ -6,5 +6,5 @@ from .views import receive_mail, MailListView, MailView
 urlpatterns = [
     path(os.environ.get('EXTERNAL_API_MAIL', ''), receive_mail),
     path(f"{os.environ.get('EXTERNAL_API_MAIL', '')}fetch/", MailListView.as_view()),
-    path(f"{os.environ.get('EXTERNAL_API_MAIL', '')}fetch/<int:pk>", MailView.as_view()),
+    path(f"{os.environ.get('EXTERNAL_API_MAIL', '')}fetch/<int:pk>/", MailView.as_view()),
 ]
