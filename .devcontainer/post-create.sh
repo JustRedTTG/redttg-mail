55f862507e5d593099a68e861aeb8e01043874eb
Red <red@redttg.com>
2023-11-08 17:14:03 +0000

    change the post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Copy nginx.conf to the same directory as post-create.sh
-cp "$(dirname "$0")/nginx.conf" "/etc/nginx/conf.d/default.conf"
-
-# Install nginx
 sudo apt-get update
 sudo apt-get install nginx -y
+
+cp "$(dirname "$0")/nginx.conf" "/etc/nginx/conf.d/default.conf"
