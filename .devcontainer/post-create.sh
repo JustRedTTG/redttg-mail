25549d894444187f62249274c8d9ccbe8b512b3c
Red <red@redttg.com>
2023-11-08 19:38:39 +0000

    final

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,5 +4,6 @@ sudo apt-get update
 sudo apt-get install nginx npm -y
 npm i --prefix ./redttg_mail_frontend
 pip install -r requirements.txt
+python manage.py migrate
 
 sudo cp "$(dirname "$0")/nginx.conf" "/etc/nginx/conf.d/default.conf"
